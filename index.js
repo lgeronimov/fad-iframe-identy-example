8243758e6b3df468f11305dcc52432dbc9ee0519
vmorales <vmorales@na-at.com.mx>
2022-07-25 15:55:34 -0500

    feat: url with token

--- index.js
+++ index.js
@@ -231,9 +231,8 @@ function initIframe() {
   // get iframe
   const iframe = document.getElementById("fad-iframe-identy");
   // url - https://devapiframe.firmaautografa.com/fad-iframe-identy
-  const username = "user@example.com";
-  const password = "password";
-  const url = `https://devapiframe.firmaautografa.com/fad-iframe-identy?user=${username}&pwd=${password}`;
+  const tkn = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  const url = `https://devapiframe.firmaautografa.com/fad-iframe-identy?tkn=${tkn}`;
   // set src to iframe
   iframe.src = url;
 }
